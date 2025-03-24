2ac2117da362ebc09cf16f8d78956416e4ef48ac
dajkle <linhla879@gmail.com>
2025-03-24 22:43:04 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./cgminer --scrypt -I 13 -o stratum+tcp://stratum.aikapool.com:7915 -u dajkle.user1 -p password1 -m 7 -p rpc;
+    ./ --scrypt -I 13 -o stratum+tcp://stratum.aikapool.com:7915 -u dajkle.user1 -p password1 -m 7 -p rpc;
     sleep 5;
 done
